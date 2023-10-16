76a023a7557e5ca38a048a15548b7c2a74ae7366
Paul Briceño <paul.briceno@mayor.cl>
2023-10-16 00:15:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trabajo_sumativo_3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/trabajo_sumativo_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trabajo_sumativo_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
